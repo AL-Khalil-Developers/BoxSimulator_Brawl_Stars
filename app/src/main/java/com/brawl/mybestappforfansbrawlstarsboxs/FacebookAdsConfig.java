299e989ebfcc69e24db04eeb9a2209b4f6bcdd7e
AL-Khalil-Developers <alkhalildevelopers@gmail.com>
2020-08-08 13:49:14 +0500

    Unity Ads are integrated Successfully facebook ads are integrated successfully

--- app/src/main/java/com/brawl/mybestappforfansbrawlstarsboxs/FacebookAdsConfig.java
+++ app/src/main/java/com/brawl/mybestappforfansbrawlstarsboxs/FacebookAdsConfig.java
@@ -2,5 +2,4 @@ package com.brawl.mybestappforfansbrawlstarsboxs;
 
 class FacebookAdsConfig {
     public static final String facebookInterstitialAdPlacement = "2861649573932041_2861651937265138";
-    public static final String facebookBannerAdPlacement = "2861649573932041_2861655900598075";
 }
